1523067febc68d110e6aa6b01af6cdc526db8277
rafael-alani <rafael.alani@hotmail.com>
2022-12-08 14:09:03 +0100

    added component template and styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,46 @@
 import React from "react";
 
+import { AppBar, Container, Grid, Grow, Typography } from "@material-ui/core";
+
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import memories from "./images/memories.png";
+import useStyles from "./styles";
+
 const App = () => {
-    return (
-        <div>
-            <h1>
-                App
-            </h1>
-        </div>
-    );
-}
+  const classes = useStyles();
+  return (
+    <Container maxwidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+          <img
+            className={classes.image}
+            src={memories}
+            alt="memories"
+            height="60"
+          />
+        </Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justtify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -3,14 +3,18 @@ import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/posts", postRoutes);
+
 const CONNECTION_URL =
-  "mongodb+srv://rafael:test@cluster0.ou2zfqx.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://rafael:testing123@cluster0.ou2zfqx.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
@@ -18,7 +22,11 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log(`Server running on port: ${PORT}`))
-  .catch((error) => console.log(error.message));
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set("useFindAndModify", false);
