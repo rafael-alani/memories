c78e3f2b5f45f0acc8d844f05730626a52d1fa60
rafael-alani <rafael.alani@hotmail.com>
2022-12-08 14:55:59 +0100

    added redux and thunk capabilities

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
-
 import { AppBar, Container, Grid, Grow, Typography } from "@material-ui/core";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
+import { getPosts } from "./actions/posts";
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
 import memories from "./images/memories.png";
@@ -9,6 +10,12 @@ import useStyles from "./styles";
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
     <Container maxwidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
